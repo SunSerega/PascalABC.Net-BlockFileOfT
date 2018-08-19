1dc6ac56db1218b160f329d091509182741ff826
Sun Serega <latchenko3@yandex.ru>
2018-08-19 17:30:26 +0300

    Uploaded all

---
+++ CompileAndTest.pas
@@ -0,0 +1,91 @@
+﻿procedure CompileFile(fname:string);
+begin
+  var p := new System.Diagnostics.Process;
+  p.StartInfo.FileName := 'C:\Program Files (x86)\PascalABC.NET\pabcnetc.exe';
+  p.StartInfo.Arguments := '"'+System.IO.Path.GetFullPath(fname)+'"';
+  p.StartInfo.UseShellExecute := false;
+  p.StartInfo.RedirectStandardError := true;
+  p.Start;
+  write(p.StandardError.ReadToEnd+#10*1);
+  p.WaitForExit;
+end;
+
+procedure CompileSource;
+begin
+  System.IO.Directory.EnumerateFiles('LibSource')
+  .Where(fname->System.IO.Path.GetExtension(fname)='.pas')
+  .ForEach(fname->CompileFile(fname));
+  
+  System.IO.Directory.EnumerateFiles('Lib')
+  .Where(fname->System.IO.Path.GetExtension(fname)='.pcu')
+  .ForEach(fname->System.IO.File.Delete(fname));
+  
+  System.IO.Directory.EnumerateFiles('LibSource')
+  .Where(fname->System.IO.Path.GetExtension(fname)='.pcu')
+  .ForEach(fname->System.IO.File.Move(fname,'Lib\'+System.IO.Path.GetFileName(fname)));
+  
+  write('compile ok'+#10*2)
+end;
+
+function GetReadyPcu:array of string :=
+System.IO.Directory.EnumerateFiles('Lib')
+.Where(fname->System.IO.Path.GetExtension(fname)='.pcu')
+.Select(fname->System.IO.Path.GetFileName(fname))
+.ToArray;
+
+procedure TestAll(path:string; ReadyPcu:array of string) :=
+try
+  
+  System.IO.Directory.EnumerateDirectories(path)
+  .ForEach(procedure(dir)->(new System.Threading.Thread(()->TestAll(dir, ReadyPcu))).Start);
+  
+  ReadyPcu.ForEach(fname->System.IO.File.Copy('Lib\'+fname,path+'\'+fname));
+  
+  System.IO.Directory.EnumerateFiles(path)
+  .Where(fname->System.IO.Path.GetExtension(fname)='.pas')
+  .ForEach(fname->
+  try
+    CompileFile(fname)
+  except
+    on e:Exception do
+      write($'Error compiling file {fname}{#10}{e}{#10}');
+  end);
+  
+  ReadyPcu.ForEach(fname->System.IO.File.Delete(path+'\'+fname));
+  
+  System.IO.Directory.EnumerateFiles(path)
+  .Where(fname->System.IO.Path.GetExtension(fname)='.exe')
+  .ForEach(fname->
+  try
+    var p := new System.Diagnostics.Process;
+    p.StartInfo.FileName := fname;
+    p.StartInfo.WorkingDirectory := path;
+    p.StartInfo.UseShellExecute := false;
+    p.StartInfo.RedirectStandardError := true;
+    p.Start;
+    write(p.StandardError.ReadToEnd+#10*1);
+    p.WaitForExit//(1000);
+  except
+    on e:Exception do
+      write($'Error executing file {fname}{#10}{e}{#10}');
+  end);
+  
+  write($'path {path} ok{#10*2}');
+  
+except
+  on e:Exception do
+    write($'Error testing path {path}{#10}{e}{#10}');
+end;
+
+begin
+  try
+    CompileSource;
+    var ReadyPcu := GetReadyPcu;
+    TestAll('Test',ReadyPcu);
+    //TestAll('BlockFileOfT',ReadyPcu);//ToDo потом придумать как красивее...
+    readln;
+  except
+    on e:Exception do
+      writeln(e);
+  end;
+end.
